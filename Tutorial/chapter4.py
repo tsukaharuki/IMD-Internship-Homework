6a549a94a5e9ff77c1825d922cb637da85a9ace1
st19060nk <66375889+st19060nk@users.noreply.github.com>
2024-08-29 02:21:55 +0900

    4-3

--- Tutorial/chapter4.py
+++ Tutorial/chapter4.py
@@ -3,7 +3,10 @@ import numpy as np
 
 img = np.zeros((512,512,3),np.uint8)
 print(img.shape)
-img[:] = 255,0,0
+#img[:] = 255,0,0
+
+cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
+cv2.rectangle(img,(0,0),(250,350),(0,0,255),2)
 
 cv2.imshow("Image",img)
 
