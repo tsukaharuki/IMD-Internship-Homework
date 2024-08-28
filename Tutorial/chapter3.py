e27422c5c1fc2e0fd778d895b355b4f32544aa46
st19060nk <66375889+st19060nk@users.noreply.github.com>
2024-08-29 01:40:00 +0900

    3-3

--- Tutorial/chapter3.py
+++ Tutorial/chapter3.py
@@ -4,9 +4,13 @@ import numpy as np
 img = cv2.imread("Tutorial/Resources/lambo.png")
 print(img.shape)
 
-imgResize = cv2.resize(img,(300,200))
+imgResize = cv2.resize(img,(1000,500))
+print(imgResize.shape)
+
+imgCropped = img[0:200,200:500]
 
 cv2.imshow("Image",img)
-cv2.imshow("Image Resize", imgResize)
+#cv2.imshow("Image Resize", imgResize)
+cv2.imshow("Image Cropped", imgCropped)
 
 cv2.waitKey(0)
